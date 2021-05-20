ffc31d2409928817dcafa70f0a3c014bf55ca8ce
Ariel Shadmon <78187701+finerio@users.noreply.github.com>
2021-05-20 16:10:24 +0200

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
-export const apiUrl = process.env.API_URL;
+export const apiUrl = "https://paleboard-backend.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
-export const SOCKET_ENDPOINT = process.env.SOCKET_ENDPOINT;
+export const SOCKET_ENDPOINT = "https://paleboard-backend.herokuapp.com";
