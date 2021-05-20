cb5bfc8765569378433b7f8dc1c986679db92a0c
Ariel Shadmon <78187701+finerio@users.noreply.github.com>
2021-05-20 15:40:54 +0200

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 export const apiUrl = process.env.API_URL || "http://localhost:4000";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
-export const SOCKET_ENDPOINT = "http://localhost:4000";
+export const SOCKET_ENDPOINT = process.env.SOCKET_ENDPOINT || "http://localhost:4000";
